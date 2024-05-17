b31be93ddc12c9782e5bb90e330999c8dc0f59e5
dokidokitiger <ghlee98s@daum.net>
2024-05-17 11:04:02 +0900

    1st commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,11 +1,14 @@
 // schemas/index.js
 
 import mongoose from 'mongoose';
+import dotenv, {config} from 'dotenv';
 
+dotenv.config();
+const {minsoo} = process.env;
 const connect = () => {
   mongoose
     .connect(
-      'mongodb+srv://ghlee98s:IcSiqzCkRZOX0Mjz@express-mongo.bj9ga0f.mongodb.net/?retryWrites=true&w=majority&appName=express-mongo',
+      minsoo,
       {
         dbName: 'spa_products', // sparta_products 데이터베이스명을 사용합니다.
       },
